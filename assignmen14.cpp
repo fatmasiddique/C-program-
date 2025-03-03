eafccc9f2a5a967504c8ddd567c6a62083768ee4
fatmasiddique <huma1.fatma@stu.adamasuniversity.ac.in>
2025-03-03 20:23:42 +0530

    Add files via upload

---
+++ assignmen14.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+	int main()
+	{
+		double a=8.34, b=2.90, c=5.59;
+		printf("number 1:%2lf\n",a);
+		printf("floor:%2lf\n",floor(a));
+		printf("ceiling:%2lf\n",ceil(a));
+    	printf("number 2:%2lf\n",b);
+		printf("floor:%2lf\n",floor(b));
+		printf("ceiling:%2lf\n",ceil(b));
+		printf("number 3:%2lf\n",c);
+		printf("floor:%2lf\n",floor(c));
+		printf("ceiling:%2lf\n",ceil(c));
+	}
