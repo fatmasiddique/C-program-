17ed4f2f642a14b1ddec20dcee5d0483e4d1f0ab
fatmasiddique <huma1.fatma@stu.adamasuniversity.ac.in>
2025-03-03 20:44:45 +0530

    Add files via upload

---
+++ assignment18.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+	int main()
+	{
+		int a;
+		printf("Enter the number:");
+		scanf("%d",&a);
+		if(a>=0)
+		printf("The entered number is positive.");
+		else
+		printf("The entered number is negative.");
+		return 0;
+	}
