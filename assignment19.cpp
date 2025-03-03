c45bd2a3f0918316758d8cafab340be92497e393
fatmasiddique <huma1.fatma@stu.adamasuniversity.ac.in>
2025-03-03 20:49:25 +0530

    Add files via upload

---
+++ assignment19.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+	int main()
+	{
+		int a, b;
+		printf("Enter the first number:");
+		scanf("%d",&a);
+		printf("Enter the second number:");
+		scanf("%d",&b);
+		if(a>b)
+		printf("First number is greater.");
+		else
+		printf("Second number is greater.");
+			return 0;
+	}
