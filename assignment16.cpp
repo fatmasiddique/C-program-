07e77da345b9ece25b43864627a5698c64c30fd0
fatmasiddique <huma1.fatma@stu.adamasuniversity.ac.in>
2025-03-03 20:34:44 +0530

    Add files via upload

---
+++ assignment16.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+	int main()
+	{
+		float a;
+		printf("Enter the roll number:");
+		scanf("%f",&a); 
+		if (a==40)
+		{
+			printf("Roll number 40 present.");
+		}
+		return 0;
+	}
